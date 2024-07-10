b525f598b95bb23f37ab6e63b4d7b3f7080bf6da
Ikeoluwa Atulomah <adesinaikeoluwa4@gmail.com>
2024-07-09 21:45:21 -0700

    Module 1, sprint 11 finished

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -1,43 +1,64 @@
 // 👇 YOUR WORK STARTS ON LINE 19
-import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
-import server from '../../../backend/mock-server'
-import App from '../App'
+import React from "react";
+import { render, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import server from "../../../backend/mock-server";
+import App from "../App";
 
-describe('Stranger Things App', () => {
-  let user
-  afterEach(() => { server.resetHandlers() })
-  beforeAll(() => { server.listen() })
-  afterAll(() => { server.close() })
+describe("Stranger Things App", () => {
+  let user;
+  afterEach(() => {
+    server.resetHandlers();
+  });
+  beforeAll(() => {
+    server.listen();
+  });
+  afterAll(() => {
+    server.close();
+  });
   beforeEach(() => {
-    render(<App />)
-    user = userEvent.setup()
-  })
-  test('App mounts without crashing', () => {
+    render(<App />);
+    user = userEvent.setup();
+  });
+  test("App mounts without crashing", () => {
     // 👉 TASK: print the simulated DOM using screen.debug
-  })
-  test('App renders the correct texts', async () => {
+    screen.debug();
+  });
+  test("App renders the correct texts", async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
-
+    const button = screen.getByText(/Press to Get Show Data/i);
+    await user.click(button);
     // 👉 TASK: create a waitFor and await for the following to be true:
-    //    - The text "Press to Get Show Data" is no longer in the DOM
-    //    - The text "Stranger Things" exists in the DOM
-    //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
-    //    - The text "Select A Season" exists in the DOM
-    // ❗ You will need { exact: false } to select the longer text
+    await waitFor(() => {
+      //    - The text "Press to Get Show Data" is no longer in the DOM
+      expect(screen.queryByText(/Press to Get Show Data/i)).not.toBeInTheDocument();
 
+      //    - The text "Stranger Things" exists in the DOM
+      expect(screen.getAllByText(/Stranger Things/i)).toHaveLength(2);
+
+      //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
+      expect(screen.getByText(/A love letter to the '80s classics that captivated a generation/i, { exact: false })).toBeInTheDocument();
+      //    - The text "Select A Season" exists in the DOM
+      expect(screen.getByText(/Select A Season/i)).toBeInTheDocument();
+      // ❗ You will need { exact: false } to select the longer text
+    });
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
-
+    const seasonSelect = document.querySelector("select");
+    await user.selectOptions(seasonSelect, "1");
     // 👉 TASK: create the following assertions:
-    //    - The text "Season 2, Episode 1" exists in the DOM
-    //    - The text "Chapter One: MADMAX" exists in the DOM
-    //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
-    // ❗ You will need { exact: false } to select the longer text
+    await waitFor(() => {
+      //    - The text "Season 2, Episode 1" exists in the DOM
+      expect(screen.getByText(/Season 2, Episode 1/i)).toBeInTheDocument();
 
-  })
-})
+      //    - The text "Chapter One: MADMAX" exists in the DOM
+      expect(screen.getByText(/Chapter One: MADMAX/i)).toBeInTheDocument();
+      //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
+      expect(screen.getByText(/One year after the events with the Upside Down and the Demogorgon/i, { exact: false })).toBeInTheDocument();
+      // ❗ You will need { exact: false } to select the longer text
+    });
+  });
+});
--- frontend/components/tests/Episode.test.js
+++ frontend/components/tests/Episode.test.js
@@ -1,8 +1,9 @@
 // 👇 YOUR WORK STARTS ON LINE 28
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Episode from "../Episode"
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Episode from "../Episode";
+import { debug } from "webpack";
 
 // ❗ EXAMPLE EPISODE TEST OBJECT ❗
 const exampleEpisodeData = {
@@ -16,39 +17,48 @@ const exampleEpisodeData = {
   rating: { average: 8.2 },
   runtime: 49,
   season: 1,
-  summary: "A young boy mysteriously disappears, and his panicked mother \
+  summary:
+    "A young boy mysteriously disappears, and his panicked mother \
 demands that the police find him. Meanwhile, the boy's friends conduct \
 their own search, and meet a mysterious girl in the forest.",
   type: "regular",
   url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
-}
+};
 
-describe('Episode component', () => {
+describe("Episode component", () => {
   test("renders without error", () => {
     // 👉 TASK: render the component passing episode data
-
+    render(<Episode episode={exampleEpisodeData} />);
     // 👉 TASK: print the simulated DOM using screen.debug
-
-  })
+    screen.debug();
+  });
   test("renders texts and alt texts correctly", () => {
     // 👉 TASK: render the component passing episode data and getting the rerender utility
+    const { rerender } = render(<Episode episode={exampleEpisodeData} />);
 
     // 👉 TASK: check that the summary renders to the DOM
+    expect(screen.getByText(/A young boy mysteriously disappears/i)).toBeInTheDocument();
 
     // 👉 TASK: check that the alt text "episode image" is present
+    expect(screen.getByAltText(/episode image/i)).toBeInTheDocument();
 
     // 👉 TASK: rerender the component passing episode data lacking an image
+    rerender(<Episode episode={{ ...exampleEpisodeData, image: null }} />);
+
     // ❗ Study the Episode component to understand what happens in this case
-
     // 👉 TASK: check that the default image appears in the DOM
-    // ❗ Use querySelector to select the image by its src attribute
+    expect(screen.getByAltText(/episode image/i)).toBeInTheDocument();
 
+    // ❗ Use querySelector to select the image by its src attribute
     // 👉 TASK: check that the "generic episode image" alt text is present
+    const img = document.querySelector('img[src="https://i.ibb.co/2FsfXqM/stranger-things.png"]');
+    expect(img).toHaveAttribute("alt", "generic episode image");
 
     // 👉 TASK: rerender the component passing an undefined episode
+    rerender(<Episode episode={undefined} />);
+
     // ❗ Study the Episode component to understand what happens in this case
-
     // 👉 TASK: check that the "Loading episode..." text is present
-
-  })
-})
+    expect(screen.getByText(/Loading episode.../i)).toBeInTheDocument();
+  });
+});
